a2a8fbb7ec115670591b14391d94fb1c5a54d58f
prudencio <prudencio.vepa@gmail.com>
2019-02-07 15:21:25 -0800

    Cambio de diseno de login

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,251 +1,80 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistema de Producción Woori</title>
-  <!-- Bootstrap core CSS -->
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!-- Include the above in your HEAD tag -->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sistema de Producción Woori</title>
+        <!-- plugins:css -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/vendor.bundle.base.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/vendor.bundle.addons.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/style.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/login/css/materialdesignicons.min.css'); ?>">
+        <!-- SweetAlert -->
+        <script src="<?php echo base_url(); ?>/assets/js/sweetalert2/dist/sweetalert2.min.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/js/sweetalert2/dist/sweetalert2.min.css">
+    </head>
 
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-  <!-- SweetAlert -->
-  <script src="<?php echo base_url();?>/assets/js/sweetalert2/dist/sweetalert2.min.js"></script>
-  <link rel="stylesheet" href="<?php echo base_url();?>/assets/js/sweetalert2/dist/sweetalert2.min.css">
-</head>
-<style type="text/css">
- @charset "utf-8";
+    <body>
+        <div class="container-scroller">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="content-wrapper auth p-0 theme-two">
+                    <div class="row d-flex align-items-stretch">
+                        <div class="col-md-4 banner-section d-none d-md-flex align-items-stretch justify-content-center">
+                            <div class="slide-content bg-1"> </div>
+                        </div>
+                        <div class="col-12 col-md-8 h-100 bg-white">
+                            <div class="auto-form-wrapper d-flex align-items-center justify-content-center flex-column">
+                                <div class="nav-get-started">
 
+                                    <?php if (isset($_SESSION['err'])): ?>
+                                        <script>
+                                            swal({
+                                                type: 'error',
+                                                title: 'Oops...',
+                                                text: '<?= $this->session->userdata('err'); ?>',
+                                                footer: ''
+                                            })
 
-@import url//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css);
+                                        </script>
 
-
-
-div.main{
-    background: #0264d6; /* Old browsers */
-background: -moz-radial-gradient(center, ellipse cover,  #0264d6 1%, #1c2b5a 100%); /* FF3.6+ */
-background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(1%,#0264d6), color-stop(100%,#1c2b5a)); /* Chrome,Safari4+ */
-background: -webkit-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Chrome10+,Safari5.1+ */
-background: -o-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* Opera 12+ */
-background: -ms-radial-gradient(center, ellipse cover,  #0264d6 1%,#1c2b5a 100%); /* IE10+ */
-background: radial-gradient(ellipse at center,  #0264d6 1%,#1c2b5a 100%); /* W3C */
-filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0264d6', endColorstr='#1c2b5a',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-height:calc(100vh);
-width:100%;
-}
-
-[class*="fontawesome-"]:before {
-  font-family: 'FontAwesome', sans-serif;
-}
-
-/* ---------- GENERAL ---------- */
-
-* {
-  box-sizing: border-box;
-    margin:0px auto;
-
-  &:before,
-  &:after {
-    box-sizing: border-box;
-  }
-
-}
-
-body {
-   
-    color: #606468;
-  font: 87.5%/1.5em 'Open Sans', sans-serif;
-  margin: 0;
-}
-
-a {
-  color: #eee;
-  text-decoration: none;
-}
-
-a:hover {
-  text-decoration: underline;
-}
-
-input {
-  border: none;
-  font-family: 'Open Sans', Arial, sans-serif;
-  font-size: 14px;
-  line-height: 1.5em;
-  padding: 0;
-  -webkit-appearance: none;
-}
-
-p {
-  line-height: 1.5em;
-}
-
-.clearfix {
-  *zoom: 1;
-
-  &:before,
-  &:after {
-    content: ' ';
-    display: table;
-  }
-
-  &:after {
-    clear: both;
-  }
-
-}
-
-.container {
-  left: 50%;
-  position: fixed;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-
-/* ---------- LOGIN ---------- */
-
-#login form{
-  width: 250px;
-}
-#login, .logo{
-    display:inline-block;
-    width:40%;
-}
-#login{
-border-right:1px solid #fff;
-  padding: 0px 22px;
-  width: 59%;
-}
-.logo{
-color:#fff;
-font-size:50px;
-  line-height: 125px;
-}
-
-#login form span.fa {
-  background-color: #fff;
-  border-radius: 3px 0px 0px 3px;
-  color: #000;
-  display: block;
-  float: left;
-  height: 50px;
-    font-size:24px;
-  line-height: 50px;
-  text-align: center;
-  width: 50px;
-}
-
-#login form input {
-  height: 50px;
-}
-fieldset{
-    padding:0;
-    border:0;
-    margin: 0;
-
-}
-#login form input[type="text"], input[type="password"] {
-  background-color: #fff;
-  border-radius: 0px 3px 3px 0px;
-  color: #000;
-  margin-bottom: 1em;
-  padding: 0 16px;
-  width: 200px;
-}
-
-#login form input[type="submit"] {
-  border-radius: 3px;
-  -moz-border-radius: 3px;
-  -webkit-border-radius: 3px;
-  background-color: #000000;
-  color: #eee;
-  font-weight: bold;
-  /* margin-bottom: 2em; */
-  text-transform: uppercase;
-  padding: 5px 10px;
-  height: 30px;
-}
-
-#login form input[type="submit"]:hover {
-  background-color: #d44179;
-}
-
-#login > p {
-  text-align: center;
-}
-
-#login > p span {
-  padding-left: 5px;
-}
-.middle {
-  display: flex;
-  width: 600px;
-}
-.imglogo{
-
-  -webkit-filter: drop-shadow(1px 1px 0 white)
-                  drop-shadow(-1px -1px 0 white);
-  filter: drop-shadow(1px 1px 0 white) 
-          drop-shadow(-1px -1px 0 white);
-
-  padding-left: 20px;
-
-}
-</style>
-<body  >
-  
-   
-    <?php if(isset($_SESSION['err'])): ?>
-      <script>
-        swal({
-          type: 'error',
-          title: 'Oops...',
-          text: '<?= $this->session->userdata('err'); ?>',
-          footer: ''
-        })
-      </script>
-    <?php endif; ?>
-  <div class="main">
-    
-    
-    <div class="container">
-<center>
-<div class="middle">
-      <div id="login">
-  <form method="POST"  action="<?= base_url('welcome/login') ?>" >
-
-          <fieldset class="clearfix">
-
-            <p ><span class="fa fa-user"></span><input type="text"   name="usuario" Placeholder="Usuario" required autocomplete="off" ></p> <!-- JS because of IE support; better: placeholder="Username" -->
-            <p><span class="fa fa-lock"></span><input type="password"    name="password" Placeholder="Contraseña" required autocomplete="off"></p> <!-- JS because of IE support; better: placeholder="Password" -->
-            
-             <div align="right">
-             <span style="width:50%; text-align:right;  display: inline-block;">
-              <input type="submit" value="Entrar">
-            </span>
+                                    <?php endif ?>
+                                </div>
+                                <form method="POST"  action="<?= base_url('welcome/login') ?>" >
+                                    <h3 class="mr-auto">Hola! bienvenido al Sistema de Produccón Woori</h3>
+                                    <p class="mb-5 mr-auto">Ingrese sus datos.</p>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="mdi mdi-account-outline"></i>
+                                                </span>
+                                            </div>
+                                            <input type="text" name="usuario" class="form-control" placeholder="Usuario" required="required"> </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="mdi mdi-lock-outline"></i>
+                                                </span>
+                                            </div>
+                                            <input type="password" name="password" class="form-control" placeholder="Contraseña" required="required"> </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button class="btn btn-primary submit-btn" type="submit">Entrar</button>
+                                    </div>
+                                    <div class="wrapper mt-5 text-gray">
+                                        <p class="footer-text">Copyright © <?php echo date("Y"); ?> Proteges. Todos los derechos reservados.</p> 
+                                    </div>
+                                </form>
                             </div>
-
-          </fieldset>
-<div class="clearfix"></div>
-        </form>
-
-        <div class="clearfix"></div>
-
-      </div> <!-- end login -->
-      <div class="logo"> <img src="<?php echo base_url();?>/assets/images/woorilogo.png" width="150%" class="img-fluid imglogo" alt="Responsive image">
-          
-          <div class="clearfix"></div>
-      </div>
-      
-      </div>
-</center>
-    </div>
-
-</div>
-</body>
+                        </div>
+                    </div>
+                </div> 
+            </div> 
+        </div> 
+    </body> 
 </html>
