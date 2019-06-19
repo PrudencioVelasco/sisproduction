0e47a7148a55340c1774aa62309773c4c28359ac
Prudencio <prudencio.vepa@gmail.com>
2019-06-19 16:21:54 -0700

    Transferencia

---
+++ application/views/transferencia/detalle.php
@@ -0,0 +1,253 @@
+
+
+
+
+ <!-- page content -->
+      <div class="right_col" role="main">
+
+        <div class="">
+          
+          <div class="clearfix"></div>
+
+          <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+                  <div class="row">
+                    <div class="col-md-6" align="left">
+                       <h2><strong>Agregar Número de Parte</strong></h2>
+                    </div>
+                    <div class="col-md-6" style="display: flex; justify-content: flex-end">
+                      <h2><strong>Transferencia: # <?php echo $id; ?></strong></h2>
+                    </div>
+                  </div>
+                  
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <div id="app">
+                     <div class="container">
+                        <div class="row">
+                          <div class="col-md-12">
+                          <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Large modal</button>
+
+                          <div class="modal fade bd-example-modal-lg"   role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg">
+                              <div class="modal-content">
+                                 <!-- Modal Header -->
+                                    <div class="modal-header">
+                                      <h4 class="modal-title">Modal Heading</h4>
+                                      <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+
+                                    <!-- Modal body -->
+                                    <div class="modal-body">
+                                      Modal body..
+                                      <form>
+                                          <div class="row">
+                                                  <div class="col-md-12">
+                                                      <div class="form-group">
+                                                          <label><font color="red">*</font> Número de Parte</label>
+                                                          <input type="text" class="form-control"  autcomplete="off"> 
+                                                      </div> 
+                                                  </div> 
+                                                  
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Seleccionar cliente</label> 
+                                            <select class="select2_single form-control ">
+                                                            <option value="AK">Alaska</option>
+                                                            <option value="HI">Hawaii</option>
+                                                            <option value="CA">California</option>
+                                                            <option value="NV">Nevada</option>
+                                                            <option value="OR">Oregon</option>
+                                                            <option value="WA">Washington</option>
+                                                            <option value="AZ">Arizona</option>
+                                                            <option value="CO">Colorado</option>
+                                                            <option value="ID">Idaho</option>
+                                                            <option value="MT">Montana</option>
+                                                            <option value="NE">Nebraska</option>
+                                                            <option value="NM">New Mexico</option>
+                                                            <option value="ND">North Dakota</option>
+                                                            <option value="UT">Utah</option>
+                                                            <option value="WY">Wyoming</option>
+                                                            <option value="AR">Arkansas</option>
+                                                            <option value="IL">Illinois</option>
+                                                            <option value="IA">Iowa</option>
+                                                            <option value="KS">Kansas</option>
+                                                            <option value="KY">Kentucky</option>
+                                                            <option value="LA">Louisiana</option>
+                                                            <option value="MN">Minnesota</option>
+                                                            <option value="MS">Mississippi</option>
+                                                            <option value="MO">Missouri</option>
+                                                            <option value="OK">Oklahoma</option>
+                                                            <option value="SD">South Dakota</option>
+                                                            <option value="TX">Texas</option>
+                                                          </select>
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Seleccionar Modelo</label> 
+                                            <select class="select2_single form-control ">
+                                                            <option value="AK">Alaska</option>
+                                                            <option value="HI">Hawaii</option>
+                                                            <option value="CA">California</option>
+                                                            <option value="NV">Nevada</option>
+                                                            <option value="OR">Oregon</option>
+                                                            <option value="WA">Washington</option>
+                                                            <option value="AZ">Arizona</option>
+                                                            <option value="CO">Colorado</option>
+                                                            <option value="ID">Idaho</option>
+                                                            <option value="MT">Montana</option>
+                                                            <option value="NE">Nebraska</option>
+                                                            <option value="NM">New Mexico</option>
+                                                            <option value="ND">North Dakota</option>
+                                                            <option value="UT">Utah</option>
+                                                            <option value="WY">Wyoming</option>
+                                                            <option value="AR">Arkansas</option>
+                                                            <option value="IL">Illinois</option>
+                                                            <option value="IA">Iowa</option>
+                                                            <option value="KS">Kansas</option>
+                                                            <option value="KY">Kentucky</option>
+                                                            <option value="LA">Louisiana</option>
+                                                            <option value="MN">Minnesota</option>
+                                                            <option value="MS">Mississippi</option>
+                                                            <option value="MO">Missouri</option>
+                                                            <option value="OK">Oklahoma</option>
+                                                            <option value="SD">South Dakota</option>
+                                                            <option value="TX">Texas</option>
+                                                          </select>
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Seleccionar Revisión</label> 
+                                            <select class="select2_single form-control ">
+                                                            <option value="AK">Alaska</option>
+                                                            <option value="HI">Hawaii</option>
+                                                            <option value="CA">California</option>
+                                                            <option value="NV">Nevada</option>
+                                                            <option value="OR">Oregon</option>
+                                                            <option value="WA">Washington</option>
+                                                            <option value="AZ">Arizona</option>
+                                                            <option value="CO">Colorado</option>
+                                                            <option value="ID">Idaho</option>
+                                                            <option value="MT">Montana</option>
+                                                            <option value="NE">Nebraska</option>
+                                                            <option value="NM">New Mexico</option>
+                                                            <option value="ND">North Dakota</option>
+                                                            <option value="UT">Utah</option>
+                                                            <option value="WY">Wyoming</option>
+                                                            <option value="AR">Arkansas</option>
+                                                            <option value="IL">Illinois</option>
+                                                            <option value="IA">Iowa</option>
+                                                            <option value="KS">Kansas</option>
+                                                            <option value="KY">Kentucky</option>
+                                                            <option value="LA">Louisiana</option>
+                                                            <option value="MN">Minnesota</option>
+                                                            <option value="MS">Mississippi</option>
+                                                            <option value="MO">Missouri</option>
+                                                            <option value="OK">Oklahoma</option>
+                                                            <option value="SD">South Dakota</option>
+                                                            <option value="TX">Texas</option>
+                                                          </select>
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Seleccionar Linea</label> 
+                                            <select class="select2_linea form-control ">
+                                                            <?php 
+                                                            foreach( $datalinea as $row){ ?>
+  <option value="<?php echo $row->idlinea?>"><?php echo $row->nombrelinea ?></option>
+                                                         <?php } ?>
+                                                          </select>
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Cantidad por pallet</label> 
+                                            <select class="select2_single form-control ">
+                                                            <option value="AK">Alaska</option>
+                                                         
+                                                          </select>
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                              <div class="row">
+                                                 <div class="col-md-12">
+                                                  <div class="form-group">
+                                                        <label><font color="red">*</font> Seleccionar cliente</label> 
+                                            
+                                                        </div>  
+                                              </div>
+                                              </div>
+                                      </form>
+                                    </div>
+
+                                    <!-- Modal footer -->
+                                    <div class="modal-footer">
+                                      <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                                    </div>
+                              </div>
+                            </div>
+                          </div> 
+                          </div>
+                        </div>
+                        <div class="row">
+
+                           <table class="table">
+                          <thead>
+                            <tr>
+                              <th scope="col">Cliente</th>
+                              <th scope="col">No. P.</th>
+                              <th scope="col">P.</th>
+                              <th scope="col">Cajas</th> 
+                              
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                        </div> 
+                     </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <!-- /page content -->
+ 
